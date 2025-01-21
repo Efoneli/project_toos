971441cb1d5de63bf046ccb0cd9cdd50ccf4db2d
efoneli <abbaefoneli@gmail.com>
2025-01-21 13:51:41 +0100

    active links

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,3 +1,25 @@
+import { Metadata } from "next"
+
+type Props = {
+    params: Promise<{ productId: string }>
+}
+
+export const generateMetadata = async ({
+    params,
+}: Props): Promise<Metadata> => {
+    const id = (await params). productId
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`iphone ${id}`)
+        }, 100)
+    })
+return {
+    title: `Product ${title}`,
+}
+)
+}
+
+
 export default async function ProductsDetails({ params, }: { params: Promise<{ productId: string }>}) {
     const productId = (await params).productId
     return(
